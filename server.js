d6bcc22846bf157b68a406cd5290d4280b52c942
Poetic-Coder <soniaatsen@yahoo.com>
2021-08-10 18:49:30 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+// var http = require ('http');
+
+// var server = http.createServer(function(req, res){
+//     req.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Welcome Sonia');
+
+// });
+
+// server.listen(3000, '127.0.0.1');
+
+// console.log('you are connected to the server at port 3000');
+
+
+
+
+
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    req.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end('you are welcome, s');
+})
+
+server.listen(3000, '127.0.0.1');
+console.log('you are finally connected at port 3000')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
